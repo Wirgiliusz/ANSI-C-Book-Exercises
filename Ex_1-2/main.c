afddfae68e464dbc6a4f9b1974eec45f444db291
Wirgiliusz <kamx98@gmail.com>
2020-08-07 13:21:19 +0200

    Fixed typo in comment

--- Ex_1-2/main.c
+++ Ex_1-2/main.c
@@ -5,5 +5,5 @@ main() {
 }
 
 // ANSWER: 
-// The backslah symbol ("\") is ommited and the used character is printed ("p"). 
+// The backslash symbol ("\") is ommited and the used character is printed ("p"). 
 // Compilator shows warning "main.c:4:12: warning: unknown escape sequence: '\p'"
