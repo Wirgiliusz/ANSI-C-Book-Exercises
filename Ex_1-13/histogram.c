bc6b2295ef68b30f9b9cc50eb73cc32ecb04007f
Wirgiliusz <kamx98@gmail.com>
2020-08-13 22:17:50 +0200

    Added define for max word length

--- Ex_1-13/histogram.c
+++ Ex_1-13/histogram.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+#define MAX_WORD_LENGTH 20
 
 int main() {
     int character;
-    int nword[20];
+    int nword[MAX_WORD_LENGTH];
 
-    for(int i=0; i<20; i++) {
+    for(int i=0; i<MAX_WORD_LENGTH; i++) {
         nword[i] = 0;
     }
 }
